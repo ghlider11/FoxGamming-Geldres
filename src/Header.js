d7d8cb683b0b191377bf97d3e66185d742a5f095
jeffersongeldres <ghlider11@gmail.com>
2021-11-30 21:10:07 -0500

    commit 30/11

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+    return (
+        <header id="main-header" className="header">
+            <h1>E-Commerce</h1>
+            <span className="material-icons">
+                shopping_cart
+            </span>
+            <img src="/logo192.png" alt="logo" />
+            <nav>
+                <a href="#">link</a>
+                <a href="#">link</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//1) Necesitamos la variable React en scope
+import React from "react"
+//const React = require("react")
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//2) Necesitamos la variable ReactDOM en scope
+import ReactDOM from "react-dom"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//3) Necesitamos una aplicacion de React
+import App from "./App"
+
+import "./estilos.css"
+
+//4) Necesitmos dibujar la aplicacion en el DOM
+ReactDOM.render( <App/> ,document.getElementById("root"))
